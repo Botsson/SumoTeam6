90441112849912477c755a04b0bf18b6aac3e00b
Jimi Tähtinen <Jimi.Taehtinen@betssongroup.com>
2015-12-03 11:36:35 +0100

    example code

--- SumoTeam6.ino
+++ SumoTeam6.ino
@@ -3,16 +3,30 @@
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
 
-ZumoReflectanceSensorArray reflectanceSensors;
+ZumoReflectanceSensorArray sensors;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
 
+#define NUM_SENSORS 6
+unsigned int sensorValues[NUM_SENSORS];
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  sensors.init();
+
+  motors.setSpeeds(0, 0);
+
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  sensors.read(sensorValues);
+
+  Serial.println(sensorValues[0]);
+
+  motors.setSpeeds(100, 100);
 
 }
