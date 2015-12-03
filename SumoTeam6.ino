39c55339d96e5aba79b53665419aff0201e06c36
Jimi Tähtinen <Jimi.Taehtinen@betssongroup.com>
2015-12-03 13:42:24 +0100

    update code with sonar sensor

--- SumoTeam6.ino
+++ SumoTeam6.ino
@@ -1,32 +1,106 @@
-#include <QTRSensors.h>
-#include <ZumoReflectanceSensorArray.h>
+#include <ZumoBuzzer.h>
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+#include <NewPing.h>
 
-ZumoReflectanceSensorArray sensors;
-ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON);
+#define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     2  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 75 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-#define NUM_SENSORS 6
-unsigned int sensorValues[NUM_SENSORS];
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  sensors.init();
-
-  motors.setSpeeds(0, 0);
-
+#define LED 13
+ 
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD  1500 // microseconds
   
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED     400 // 0 is stopped, 400 is full speed
+#define TURN_SPEED        400
+#define FORWARD_SPEED     200
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     300 // ms
+#define PUSH_SPEED        50
+ 
+ZumoBuzzer buzzer;
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+ 
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+ 
+ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 
+void waitForButtonAndCountDown()
+{
+  digitalWrite(LED, HIGH);
+  button.waitForButton();
+  digitalWrite(LED, LOW);
+   
+  // play audible countdown
+  for (int i = 0; i < 3; i++)
+  {
+    delay(1000);
+    buzzer.playNote(NOTE_G(3), 200, 15);
+  }
+  delay(1000);
+  buzzer.playNote(NOTE_G(4), 500, 15);  
+  delay(1000);
+}
+ 
+void setup()
+{
+  Serial.begin(115200);
+  // uncomment if necessary to correct motor directions
+  //motors.flipLeftMotor(true);
+  //motors.flipRightMotor(true);
+   
+  pinMode(LED, HIGH);
+   
+  waitForButtonAndCountDown();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  sensors.read(sensorValues);
+void loop()
+{
+  if (button.isPressed())
+  {
+    // if button is pressed, stop and wait for another press to go again
+    motors.setSpeeds(0, 0);
+    button.waitForRelease();
+    waitForButtonAndCountDown();
+  }
+   
 
-  Serial.println(sensorValues[0]);
+  sensors.read(sensor_values);
 
-  motors.setSpeeds(100, 100);
-
+  if(sonar.ping_cm() <= 5) {
+    motors.setSpeeds(PUSH_SPEED, PUSH_SPEED);
+  } else {
+  
+    if (sensor_values[0] < QTR_THRESHOLD)
+    {
+      // if leftmost sensor detects line, reverse and turn to the right
+      motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+      delay(REVERSE_DURATION + random(10, 500));
+      motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+      delay(TURN_DURATION + random(10, 500));
+      motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    }
+    else if (sensor_values[5] < QTR_THRESHOLD)
+    {
+      // if rightmost sensor detects line, reverse and turn to the left
+      motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+      delay(REVERSE_DURATION + random(10, 500));
+      motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+      delay(TURN_DURATION + random(10, 500));
+      motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    }
+    else
+    {
+      // otherwise, go straight
+      motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    }
+  }
 }
